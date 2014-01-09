499279a2d6071ab26cec3bfaa323eded386b4666
Ryan Farris <rfarris@gmail.com>
2014-01-09 03:05:59 -0500

    logic working + tested

---
+++ lib/life.rb
@@ -0,0 +1,67 @@
+
+
+#
+# Convert a string containing multiple lines into an array rep
+#
+def parse_board(input)
+  board = []
+  input.split(/\n/).each do |line|
+    row = line.chars.reject { |c| c =~ /\s/ }.map do |c|
+      next c.to_i if c == '0' || c == '1'
+      raise ArgumentError.new "Invalid character in board: #{c}"
+    end
+    board << row
+  end
+
+  raise ArgumentError.new "Must be at least two rows on the board" if board.length < 2
+
+  length = board[0].length
+  board[1, board.length].each_with_index { |row, i| raise ArgumentError.new "Row #{i}'s length is not the same." if row.length != length }
+
+  return board
+end
+
+
+def print_array(array)
+  array.each do |row|
+    row.each { |col| print col }
+    puts
+  end
+  puts
+end
+
+class Life
+  def initialize(board)
+    @board = board
+  end
+
+  def next_gen
+    scores = @board.map { |row| row.map { |col| 0 }} # init an array of scores the same size as the board
+    #print_array @board
+
+    @board.each_with_index do |row, i|
+      row.each_with_index do |col, j|
+        scores[i - 1][j]     += @board[i][j] if i - 1 >= 0
+        scores[i - 1][j - 1] += @board[i][j] if i - 1 >= 0 && j - 1 >= 0
+        scores[i][j - 1]     += @board[i][j] if j - 1 >= 0
+        scores[i + 1][j - 1] += @board[i][j] if i + 1 < scores.length && j - 1 >= 0
+        scores[i + 1][j]     += @board[i][j] if i + 1 < scores.length
+        scores[i + 1][j + 1] += @board[i][j] if i + 1 < scores.length && j + 1 < scores[i].length
+        scores[i][j + 1]     += @board[i][j] if j + 1 < scores[i].length
+        scores[i - 1][j + 1] += @board[i][j] if i - 1 >= 0 && j + 1 < scores[i].length
+      end
+    end
+
+    #print_array scores
+
+    @board = scores.each_with_index.map do |row, i|
+      row.each_with_index.map do |score, j|
+        next 0 if score < 2 # under population
+        next 1 if @board[i][j] == 1 && score > 1 && score < 4 # survival
+        next 0 if score > 3 # overcrowding
+        next 1 if score == 3 # reproduction
+        next 0 # stay dead
+      end
+    end
+  end
+end
---
+++ test/test_life.rb
@@ -0,0 +1,113 @@
+require 'test/unit'
+require '../lib/life'
+
+
+class TestLife < Test::Unit::TestCase
+
+  def test_parse_board
+    board_file = File.read('example_in.txt')
+    expected = [
+        [0, 1, 0, 0, 0],
+        [1, 0, 0, 1, 1],
+        [1, 1, 0, 0, 1],
+        [0, 1, 0, 0, 0],
+        [1, 0, 0, 0, 1]
+    ]
+
+    assert_equal expected, parse_board(board_file) # with whitespace
+    assert_equal expected, parse_board(board_file.gsub /\ /, "") # without whitespace
+
+    assert_raise(ArgumentError) do # bad character
+      bad_char = String.new(board_file)
+      bad_char[0] = '2'
+      parse_board bad_char
+    end
+    assert_raise(ArgumentError) { parse_board('') } # empty
+    assert_raise(ArgumentError) { parse_board(board_file + "0 1") } # one row is shorter
+  end
+
+
+  def test_given_example
+    life = Life.new parse_board(File.read('example_in.txt'))
+    expected = [
+        [0, 0, 0, 0, 0],
+        [1, 0, 1, 1, 1],
+        [1, 1, 1, 1, 1],
+        [0, 1, 0, 0, 0],
+        [0, 0, 0, 0, 0]
+    ]
+    assert_equal expected, life.next_gen
+  end
+
+  def test_under_population
+    # no neighbors
+    assert_equal [[0, 0, 0],
+                  [0, 0, 0],
+                  [0, 0, 0]], Life.new([[0, 0, 0],
+                                        [0, 1, 0],
+                                        [0, 0, 0]]).next_gen
+    # 1 neighbor
+    assert_equal [[0, 0, 0],
+                  [0, 0, 0],
+                  [0, 0, 0]], Life.new([[1, 0, 0],
+                                        [0, 1, 0],
+                                        [0, 0, 0]]).next_gen
+  end
+
+  def test_survival
+    # 2 neighbors
+    assert_equal [[0, 0, 0],
+                  [0, 1, 0],
+                  [0, 0, 0]], Life.new([[1, 0, 0],
+                                        [0, 1, 0],
+                                        [0, 0, 1]]).next_gen
+    # 3 neighbors
+    assert_equal [[1, 1, 1],
+                  [1, 1, 1],
+                  [0, 0, 0]], Life.new([[1, 1, 1],
+                                        [0, 1, 0],
+                                        [0, 0, 0]]).next_gen
+  end
+
+  def test_reproduction
+    assert_equal [[0, 0, 0],
+                  [0, 1, 0],
+                  [0, 0, 0]], Life.new([[1, 0, 1],
+                                        [0, 0, 0],
+                                        [0, 1, 0]]).next_gen
+  end
+
+  def test_overcrowding
+    # 4 neighbors
+    assert_equal [[1, 1, 1],
+                  [1, 0, 1],
+                  [1, 1, 1]], Life.new([[0, 1, 0],
+                                        [1, 1, 1],
+                                        [0, 1, 0]]).next_gen
+    # 5 neighbors
+    assert_equal [[1, 0, 1],
+                  [0, 0, 1],
+                  [1, 1, 1]], Life.new([[1, 1, 0],
+                                        [1, 1, 1],
+                                        [0, 1, 0]]).next_gen
+    # 6 neighbors
+    assert_equal [[1, 0, 1],
+                  [0, 0, 0],
+                  [1, 1, 1]], Life.new([[1, 1, 1],
+                                        [1, 1, 1],
+                                        [0, 1, 0]]).next_gen
+    # 7 neighbors
+    assert_equal [[1, 0, 1],
+                  [0, 0, 0],
+                  [1, 0, 1]], Life.new([[1, 1, 1],
+                                        [1, 1, 1],
+                                        [1, 1, 0]]).next_gen
+    # 8 neighbors
+    assert_equal [[1, 0, 1],
+                  [0, 0, 0],
+                  [1, 0, 1]], Life.new([[1, 1, 1],
+                                        [1, 1, 1],
+                                        [1, 1, 1]]).next_gen
+  end
+
+end
